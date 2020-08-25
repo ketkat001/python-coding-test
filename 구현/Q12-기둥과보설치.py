ef188aa1ce22a6900da9f3134fbd81d064c3ff2e
ketkat001 <ketkat001@gmail.com>
2020-08-25 13:03:16 +0900

    0825|일일 알고리즘|기둥과보설치

--- 구현/Q12-기둥과보설치.py
+++ 구현/Q12-기둥과보설치.py
@@ -1,3 +1,31 @@
+def possible(answer):
+    for x, y, a in answer:
+        if a == 0:  # 기둥일 경우
+            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
+                continue
+            return False
+
+        elif a == 1:  # 보 일 경우
+            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer):
+                continue
+            return False
+    return True
+
+
 def solution(n, build_frame):
-    answer = [[]]
-    return answer
+    answer = []
+    for build in build_frame:
+        x, y, a, b = build
+        if b == 0:  # 삭제하는 경우
+            answer.remove([x, y, a])
+            if not possible(answer):
+                answer.append([x, y, a])
+        elif b == 1:
+            answer.append([x, y, a])
+            if not possible(answer):
+                answer.remove([x, y, a])
+    return sorted(answer)
+
+
+print(solution(5, [[1, 0, 0, 1], [1, 1, 1, 1], [2, 1, 0, 1], [2, 2, 1, 1],
+                   [5, 0, 0, 1], [5, 1, 0, 1], [4, 2, 1, 1], [3, 2, 1, 1]]))
