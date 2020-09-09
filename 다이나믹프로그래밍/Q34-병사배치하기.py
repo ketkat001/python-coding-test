6ea4f8e904a3a394381f4a641cbf47a82b88db86
ketkat001 <ketkat001@gmail.com>
2020-09-09 14:33:21 +0900

    0909|일일알고리즘|그리디|병사 배치하기

---
+++ 다이나믹프로그래밍/Q34-병사배치하기.py
@@ -0,0 +1,8 @@
+N = int(input())
+soldier = list(map(int, input().split()))
+dp = [1] * N
+for i in range(1, N):
+    for j in range(0, i):
+        if soldier[i] < soldier[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(N-max(dp))
